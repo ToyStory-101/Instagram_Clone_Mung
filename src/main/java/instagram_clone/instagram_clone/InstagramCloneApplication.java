fbc5ac28aeaa0101f0810eea4b29fdbebc6ee5ee
김멍몽 <wjdahrrla@gmail.com>
2024-04-06 18:17:47 +0900

    feat: Member 엔티티 구현

--- src/main/java/instagram_clone/instagram_clone/InstagramCloneApplication.java
+++ src/main/java/instagram_clone/instagram_clone/InstagramCloneApplication.java
@@ -2,8 +2,10 @@ package instagram_clone.instagram_clone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class InstagramCloneApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/instagram_clone/instagram_clone/domain/member/entity/Member.java
@@ -0,0 +1,23 @@
+package instagram_clone.instagram_clone.domain.member.entity;
+
+import instagram_clone.instagram_clone.global.common.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id; // 고유 식별자
+
+    private String memberEmail;
+    private String memberName;
+    private String memberPassword;
+    private String memberPhone;
+    private String memberImg;
+}
